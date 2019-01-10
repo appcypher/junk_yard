5836b0fe3e79a2ce923ef23d9226a8ac2ded3f8e
Steve Akinyemi <appcypher@outlook.com>
2019-01-10 14:08:47 +0100

    Add ctypes example

---
+++ c/ctypes_example.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void print_number(int num) {
+    printf("number = %d\n", num);
+}
+
+void *get_address() {
+    void *address = (void *) print_number;
+    printf("print_number = %p\n", address);
+    return address;
+}
+
+// clang ctypes_example.c -dynamiclib -o libctypes_example.dylib
+
+// from ctypes import cdll, c_uint, c_void_p, CFUNCTYPE
+
+// # Load library and get library
+// lib = cdll.LoadLibrary('./libctypes_example.so')
+
+// # Get get_address function
+// get_address = lib.get_address
+// get_address.restype = c_void_p # print_number address
+// print_number_addr = get_address()
+// print_number = CFUNCTYPE(None, c_uint)(print_number_addr)
+
+// # Call
+// print_number(560)
+
