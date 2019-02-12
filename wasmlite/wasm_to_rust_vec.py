798923fa333110c85b92ec1ec3a2203a5bc31a73
Steve Akinyemi <appcypher@outlook.com>
2019-02-12 19:32:14 +0100

    Refactor wasm_to_rust_vec.py

--- wasmlite/wasm_to_rust_vec.py
+++ wasmlite/wasm_to_rust_vec.py
@@ -1,21 +1,19 @@
 
-import sys 
+import sys
 import os
 
 def convert_to_rust_vec(filename):
-    print("Hello world!")
     file_size = os.path.getsize(filename)
     wasm_file = open(wasm_filename, 'rb')
     vec = "vec![\n"
-    for i in range(file_size): 
+    for i in range(file_size):
         byte = wasm_file.read(1)
-        print(byte)
         vec += f"    {int.from_bytes(byte, byteorder='little'):#04x},\n"
     vec += "\n]"
     print(f"=== output ===\n", vec)
     return vec
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         wasm_filename = f"./{sys.argv[1]}.wasm"
         convert_to_rust_vec(wasm_filename)
